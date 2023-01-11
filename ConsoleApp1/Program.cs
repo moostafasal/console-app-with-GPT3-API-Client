e8c468d0cbe9b420b49522ed450cd8cdedfe2140
mostafa Salem <Mostafa salem>
2023-01-11 03:59:18 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+using Newtonsoft.Json;
+if (args.Length > 0)
+{
+    HttpClient client = new HttpClient();
+
+    client.DefaultRequestHeaders.Add("authorization", "Bearer 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");
+
+    var content = new StringContent("{\"model\": \"text-davinci-001\", \"prompt\": \"" + args[0] + "\",\"temperature\": 1,\"max_tokens\": 100}",
+        Encoding.UTF8, "application/json");
+
+    HttpResponseMessage response = await client.PostAsync("https://api.openai.com/v1/completions", content);
+
+    string responseString = await response.Content.ReadAsStringAsync();
+try
+{
+    var dyData = JsonConvert.DeserializeObject<dynamic>(responseString);
+
+    Console.WriteLine(dyData?.choices[0].text);
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine($" Could not deserialize the JSON: {ex.Message}");
+}
+}
+else
+{
+    Console.WriteLine("Fals");
+}
+
+
